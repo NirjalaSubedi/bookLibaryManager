5b54c0c6db678fc9fcdf90f83240ed49030c6e26
Nirjala Subedi <nirjalasubedi944@gmail.com>
2025-12-27 14:12:48 +0545

    Add createbook endpoint to handle book creation with validation

--- backend/app.js
+++ backend/app.js
@@ -11,5 +11,29 @@ app.listen(3000,()=>{
     console.log("running at 3000 port");
 })
 
+app.post("/createbook",async(req,res)=>{
+    const {title,author,description,isAvailable,createdAt}=req.body;
+    console.log(title,author,description,isAvailable,createdAt)
+    if(!title||!author||!isAvailable||!createdAt){
+        return res.status(400).json({
+            message:"enter all required fields"
+        });
+    }
+
+    const response=await Book.create({
+        title:title,
+        author:author,
+        description:description,
+        isAvailable:isAvailable,
+        createdAt:createdAt
+    });
+
+    res.status(200).json({
+        message:"book crested successfully",
+        data:req.body
+    })
+})
+
+
 
 
