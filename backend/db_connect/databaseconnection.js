b70205551ebd53ee260a9faaad446e3881978f88
Nirjala Subedi <nirjalasubedi944@gmail.com>
2025-12-27 13:05:17 +0545

    Refactor database connection logic to improve error handling and connection success logging

--- backend/db_connect/databaseconnection.js
+++ backend/db_connect/databaseconnection.js
@@ -4,7 +4,19 @@ exports.databaseconnection= async()=>{
     const pass=process.env.MONGO_PASS||'nirjala#2233';
     const host = process.env.MONGO_HOST || 'cluster0.zni3nwp.mongodb.net';
     const options = process.env.MONGO_OPTIONS || '?appName=Cluster0';
-    
+
     const safeuser=encodeURIComponent(user);
     const safepass=encodeURIComponent(pass);
+    const dbname='bookLibraryManagement';
+
+    const uri=`mongodb+srv://${safeuser}:${safepass}@${host}/${dbname}${options}`;
+
+    try{
+       await  mongoose.connect(uri);
+       console.log(`database connection success ${dbname}`);
+    }
+    catch(err){
+        console.error('failed to connect database',err.message);
+        throw err;
+    }
 }
